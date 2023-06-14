1960deef07f1927e51a826d45612dd4bdc5e7704
andrekipphard <andrekipphard@gmail.com>
2023-06-14 13:54:55 +0200

    leadform new images, responsiove

--- template-parts/layouts/leadform.php
+++ template-parts/layouts/leadform.php
@@ -1,5 +1,5 @@
-<div class="container w-75">
-    <div class="row py-5">
+<div class="container w-75 py-5">
+    <div class="row py-5 pb-lg-0 leadform-hero">
         <div class="col">
             <div class="h2 text-upper text-center">Ready für True Coffee?</div>
             <div class="p text-center">
@@ -15,28 +15,49 @@
                         Wie viele Personen können sich über echten Kaffee freuen?
                     </div>
                     <div class="row pt-5">
-                        <div class="col-4">
-                            <div class="form-check text-center">
-                                <input class="form-check-input d-none" type="radio" name="imgbackground" id="eins-bis-neunzehn" value="">
-                                <label for="eins-bis-neunzehn">
-                                    <img src="/wp-content/uploads/2023/03/leadform_1_19.png" alt="1-19">
-                                </label>
+                        <div class="col-12 col-lg-4 mb-5 mb-lg-0 d-flex justify-content-center align-items-center">
+                            <div class="border border-dark h-100 leadform-box">
+                                <div class="form-check d-flex flex-column align-items-center" style="height: 100%;">
+                                    <div class="flex-grow-1 d-flex flex-column align-items-center justify-content-center">
+                                        <input class="form-check-input d-none" type="radio" name="imgbackground" id="eins-bis-neunzehn" value="">
+                                        <label class="align-self-center" for="eins-bis-neunzehn">
+                                            <img src="/wp-content/uploads/2023/06/1truecoffee_leadgenerator-1-1.png" alt="1-19" class="img-fluid">
+                                        </label>
+                                    </div>
+                                    <div class="h4 text-uppercase mt-auto">
+                                    1 - 19
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div class="col-4">
-                            <div class="form-check text-center">
-                                <input class="form-check-input d-none" type="radio" name="imgbackground" id="zwanzig-bis-fuenfzig" value="">
-                                <label for="zwanzig-bis-fuenfzig">
-                                    <img src="/wp-content/uploads/2023/03/leadform_20_50.png" alt="20-50">
-                                </label>
+                        <div class="col-12 col-lg-4 mb-5 mb-lg-0 d-flex justify-content-center align-items-center">
+                            <div class="border border-dark h-100 leadform-box">
+                                <div class="form-check d-flex flex-column align-items-center" style="height: 100%;">
+                                    <div class="flex-grow-1 d-flex flex-column align-items-center justify-content-center">
+                                        <input class="form-check-input d-none" type="radio" name="imgbackground" id="zwanzig-bis-fuenfzig" value="">
+                                        <label class="align-self-center" for="zwanzig-bis-fuenfzig">
+                                            <img src="/wp-content/uploads/2023/06/3truecoffee_leadgenerator-1-1.png" alt="20-50" class="img-fluid">
+                                        </label>
+                                    </div>
+                                    <div class="h4 text-uppercase mt-auto">
+                                    20 - 50
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div class="col-4">
-                            <div class="form-check text-center">
-                                <input class="form-check-input d-none" type="radio" name="imgbackground" id="mehr-als-fuenfzig" value="">
-                                <label for="mehr-als-fuenfzig">
-                                    <img src="/wp-content/uploads/2023/03/leadform_mehr_als_50.png" alt="Mehr als 50">
-                                </label>
+                        <div class="col-12 col-lg-4 mb-5 mb-lg-0 d-flex justify-content-center align-items-center">
+                            <div class="border border-dark h-100 leadform-box">
+                                <div class="form-check d-flex flex-column align-items-center" style="height: 100%;">
+                                    <div class="flex-grow-1 d-flex flex-column align-items-center justify-content-center">
+                                        <input class="form-check-input d-none" type="radio" name="imgbackground" id="mehr-als-fuenfzig" value="">
+                                        <label class="align-self-center" for="mehr-als-fuenfzig">
+                                            <img src="/wp-content/uploads/2023/06/2truecoffee_leadgenerator-1-1.png" alt="Mehr als 50" class="img-fluid">
+                                        </label>
+                                    </div>
+                                    <div class="h4 text-uppercase mt-auto">
+                                    Mehr als 50
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -47,28 +68,49 @@
                         Wie viele Kaffeeautomaten werden benötigt?
                     </div>
                     <div class="row pt-5">
-                        <div class="col-4">
-                            <div class="form-check text-center">
-                                <input class="form-check-input d-none" type="radio" name="imgbackground" id="eine" value="">
-                                <label for="eine">
-                                    <img src="/wp-content/uploads/2023/05/Bildschirm­foto-2023-05-05-um-14.10.24.png" alt="Eine">
-                                </label>
+                        <div class="col-12 col-lg-4 mb-5 mb-lg-0 d-flex justify-content-center align-items-center">
+                            <div class="border border-dark h-100 leadform-box">
+                                <div class="form-check d-flex flex-column align-items-center" style="height: 100%;">
+                                    <div class="flex-grow-1 d-flex flex-column align-items-center justify-content-center">
+                                        <input class="form-check-input d-none" type="radio" name="imgbackground" id="eine" value="">
+                                        <label class="align-self-center" for="eine">
+                                            <img src="/wp-content/uploads/2023/06/1_maschine-1.png" alt="Eine" class="img-fluid">
+                                        </label>
+                                    </div>
+                                    <div class="h4 text-uppercase mt-auto">
+                                        1
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div class="col-4">
-                            <div class="form-check text-center">
-                                <input class="form-check-input d-none" type="radio" name="imgbackground" id="zweibisfunf" value="">
-                                <label for="zweibisfunf">
-                                    <img src="/wp-content/uploads/2023/05/Bildschirm­foto-2023-05-05-um-14.10.36.png" alt="Zwei bis Fünf">
-                                </label>
+                        <div class="col-12 col-lg-4 mb-5 mb-lg-0 d-flex justify-content-center align-items-center">
+                            <div class="border border-dark h-100 leadform-box">
+                                <div class="form-check d-flex flex-column align-items-center" style="height: 100%;">
+                                    <div class="flex-grow-1 d-flex flex-column align-items-center justify-content-center">
+                                        <input class="form-check-input d-none" type="radio" name="imgbackground" id="zweibisfunf" value="">
+                                        <label class="align-self-center" for="zweibisfunf">
+                                            <img src="/wp-content/uploads/2023/06/maschinen-ansammlung-neu-1.png" alt="Zwei bis Fünf" class="img-fluid">
+                                        </label>
+                                    </div>
+                                    <div class="h4 text-uppercase mt-auto">
+                                        2 - 5
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div class="col-4">
-                            <div class="form-check text-center">
-                                <input class="form-check-input d-none" type="radio" name="imgbackground" id="mehralsfunf" value="">
-                                <label for="mehralsfunf">
-                                    <img src="/wp-content/uploads/2023/05/Bildschirm­foto-2023-05-05-um-14.10.42.png" alt="Mehr als Fünf">
-                                </label>
+                        <div class="col-12 col-lg-4 mb-5 mb-lg-0 d-flex justify-content-center align-items-center">
+                            <div class="border border-dark h-100 leadform-box">
+                                <div class="form-check d-flex flex-column align-items-center" style="height: 100%;">
+                                    <div class="flex-grow-1 d-flex flex-column align-items-center justify-content-center">
+                                        <input class="form-check-input d-none" type="radio" name="imgbackground" id="mehralsfunf" value="">
+                                        <label class="align-self-center" for="mehralsfunf">
+                                        <img src="/wp-content/uploads/2023/06/maschinen-ansammlung-fuenf-1.png" alt="Mehr als Fünf" class="img-fluid">
+                                        </label>
+                                    </div>
+                                    <div class="h4 text-uppercase mt-auto">
+                                        Mehr als 5
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -80,15 +122,15 @@
                         Du hast es geschafft! Bei wem dürfen wir uns melden?
                     </div>
                     <div class="row pt-5">
-                        <div class="col-6 d-flex justify-content-center align-items-center">
+                        <div class="col-12 col-lg-6 d-flex justify-content-center align-items-center">
                             <img src="/wp-content/uploads/2023/03/leadform_coffee.png">
                         </div>
-                        <div class="col-6">
+                        <div class="col-12 col-lg-6">
                             <p><input type="text" placeholder="Firma"></p>
                             <p><input type="text" placeholder="Name"></p>
                             <p><input type="email" placeholder="E-Mail"></p>
-                            <p><input type="number" placeholder="Telefonnummer"></p>
-                            <button type="submit" class="btn btn-primary mt-5">Senden</button>
+                            <p><input class="leadform-phone-field" type="number" placeholder="Telefonnummer" style="padding-left:20px!important; padding-right:20px!important;"></p>
+                            <button type="submit" class="btn btn-primary mt-5 leadform-submit">Senden</button>
                         </div>
                     </div>
                     <div class="h4 text-center mt-5">Frage 3/3</div>
